8a116022d4e70a4676681cc7f4c3c0e2553a6215
chijioke100 <chijiokee31@gmail.com>
2024-10-08 07:41:37 +0000

    add validation check for 13 values

--- run.py
+++ run.py
@@ -20,7 +20,23 @@ def get_sales_data():
     print('The data should be 13 and separated by comma. ')
 
     data_string = input('Enter your data: ')
-    print(f'The data provided is: {data_string}')
+    #print(f'The data provided is: {data_string}')
     sales_data = data_string.split(',') #Remove commas from the string
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Convert the string to integers, raise valueerror if the string 
+    cannot be converted into integers and if the values are not equal to 13
+    exactly.
+    """  
+    try:
+        if len(values) != 13:
+            raise ValueError(
+                f'13 values required, provided values is {len(values)}'
+            )
+    except ValueError as ve:
+        print(f'Invalid data: {ve}, Put 13 values.\n')        
 
 get_sales_data()
